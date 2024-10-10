eeea0a05d2a52d98056dbc6f4c05f953043dfbb8
bradyDarren <dcgbfinancial@gmail.com>
2024-10-10 16:45:23 -0500

    addition of the while loop

--- caesar_cypher.py
+++ caesar_cypher.py
@@ -5,22 +5,22 @@ alpha_list = list(aplha_values)
 
 # test line
 # print(alpha_list)
-print(alpha_list[0])
-print(alpha_list[1])
 
 def encode(message, shift_number):
     encoded_message = ""
     for letter in message:
         if letter in alpha_list: 
             encoded_index = alpha_list.index(letter) + shift_number
+            if encoded_index > len(alpha_list):
+                encoded_index = encoded_index - len(alpha_list)
             encoded_message += alpha_list[encoded_index]
         elif letter == " ":
             encoded_message += " "
     return encoded_message
 
 
-# test line
-# print(encode("hello good sir",5))
+# # test line
+# print(encode("hello good sir",30))
 
 def decode(message, shift_number):
     decoded_message = "" 
@@ -33,15 +33,34 @@ def decode(message, shift_number):
     return decoded_message
 
 # test line
-# print(decode("mjqqt ltti xnw",5))
+# print(decode("lipps kssh wmv",30))
 
-def start(): 
-    user_choice = input("Would you like to encode or decode a message? : ").lower()
-    if user_choice == "encode":
-        user_message = input("Type your message: ")
-        user_shift = int(input("Type the shift number: "))
-        result = encode(user_message, user_shift)
-        return print(f"Here is your encoded result: {result}")
+def start():
+    go_again = "yes"
+    while go_again == "yes":
+        user_choice = input("Type 'encode' to encrypt, type 'decode' to decrypt: ").lower()
+        if user_choice == "encode":
+            user_message = input("Type your message: ")
+            user_shift = int(input("Type the shift number: "))
+            result = encode(user_message, user_shift)
+            print(f"Here is your encoded result: {result}")
+            go_again = input("Type 'yes' if you want to go agian. Otherwise type 'no'.").lower()
+            if go_again != "yes" or go_again != "no":
+                print("Please pick a appropriate selection")
+        elif user_choice == "decode":
+            user_message = input("Type your message: ")
+            user_shift = int(input("Type the shift number: "))
+            result = decode(user_message, user_shift)
+            print(f"Here is your decoded result: {result}")
+            go_again = input("Type 'yes' if you want to go agian. Otherwise type 'no'.").lower()
+        else:
+            print("Please pick a appropriate selection")
+            continue
+            
         
+
 start()
+
+# look into the following - what happens when a person inputs a value that exceeds the 26 letters of the alphabet? 
+
 
